934fa9a069c44817c63892c4e928e2a4897cdf40
Carlos Robles <carlos.robles97@gmail.com>
2025-11-10 13:03:34 -0600

    Initial commit

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDV3zgZnCHbabRfSPKB6qYMvP3oPE4oyWc",
+  authDomain: "company-directory-50369.firebaseapp.com",
+  projectId: "company-directory-50369",
+  storageBucket: "company-directory-50369.firebasestorage.app",
+  messagingSenderId: "1070818175477",
+  appId: "1:1070818175477:web:7b4f56c722f2bd7ef418bc"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
---
+++ src/router.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/views/HomePage.vue'
+import CardDetails from './views/CardDetails.vue'
+import LoginPage from './views/LoginPage.vue'
+import SettingsPage from './views/SettingsPage.vue'
+
+import { useAuth } from './composables/useAuth'
+import { query } from 'firebase/firestore'
+const {isAuthenticated} = useAuth()
+
+const routes = [
+  { path: '/company-directory', name: 'Home', component: HomePage },
+  { path: '/company-directory/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/company-directory/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/company-directory/Login', name: 'LoginPage', component: LoginPage },
+  { path: '/company-directory/settings', name: 'SettingsPage', component: SettingsPage, meta: {reqiuresAuth: true} },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+//navigation guard
+router.beforeEach((to, _, next) => {
+  if (to.meta.reqiuresAuth && !isAuthenticated.value) {
+    next({ name: 'LoginPage', query: {redirect: to.fullPath}})
+  }
+  else {
+    next()
+  }
+})
+
+export default router

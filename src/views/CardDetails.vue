7336b82b779369f6c7a38667390d71a854560266
Carlos Robles <carlos.robles97@gmail.com>
2025-11-10 16:24:42 -0600

    Input form completed

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -6,7 +6,7 @@ import { faker } from '@faker-js/faker';
 
 const { fetchEmployee, currentEmployee } = useAPI()
 
-onMounted( async() => {
+onMounted(async() => {
     await fetchEmployee(route.params.id)
 })
 
@@ -20,13 +20,13 @@ const route = useRoute()
 <template>
     <!-- {{ route.params.id }} -->
     <main>
-        <div v-if="currentEmployee != null" class="flex flex-col items-center justify-center gap-6">
+        <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
             <h1 class="text-6xl font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
             <h1 class="text-3xl p-5">{{ currentEmployee.title }}</h1>
             <h1 class="text-2xl p-5">{{ currentEmployee.userName }} @southtexascollege.edu</h1>
             <h1 class="text-2xl p-5"> {{ currentEmployee.qoute }}</h1>
 
-            <img class="p-8" :src="faker.image.urlLoremFlickr({category: 'cat'})" />
+            <img class="p-8" :src="faker.image.urlLoremFlickr({category: 'cat'})"></img>
 
         </div>
     </main>
